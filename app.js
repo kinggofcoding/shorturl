111fe77634b9635e4bf8b5faa6d01edae5af4b8f
jay <intuitionpz@gmail.com>
2023-12-02 23:53:30 +0800

    build index page

--- app.js
+++ app.js
@@ -7,3 +7,12 @@ app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000')
+})

7b4f7279709ee17f618c5bdfb293476ce079c9c9
jay <intuitionpz@gmail.com>
2023-12-03 14:46:54 +0800

    build copy url function

--- utils/randomGenerator.js
+++ utils/randomGenerator.js
@@ -8,9 +8,9 @@ module.exports = (resultLength) => {
     //產生亂數 Index
     const randomIndex = Math.floor(Math.random() * CHAR_COUNT)
     //依照亂數Index取出字元
-    const chooseChar = BASE_CHAR[randomIndex]
+    const randomChar = BASE_CHAR[randomIndex]
     //將取出字元組合在一起
-    result += chooseChar
+    result += randomChar
   }
   
   return result

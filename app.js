a31ed134455e5371f030542a9b2ec64a299e7d94
jay <intuitionpz@gmail.com>
2023-12-03 13:35:57 +0800

    build route:shorturl

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
 const express = require('express')
 const app = express()
 const { engine } = require('express-handlebars')
-const urlData = require('./public/urlData.json') || []
 const randomGenerator = require('./utils/randomGenerator')
 const fs = require('fs')
-
+const path = require('path')
+app.use(express.urlencoded({ extended: true }))
 app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 app.use(express.static('public'))
 
-
+const urlDataPath = path.join(__dirname, 'urlData.json')
 
 app.get('/', (req, res) => {
-    res.render('index')
+  res.render('index')
 })
 
+app.post('/shorturl', (req, res) => {
+  const { url } = req.body
+  const urlData = fs.existsSync(urlDataPath) ? JSON.parse(fs.readFileSync(urlDataPath, 'utf-8')) : []
+  const id = checkRandomId(urlData)
+
+  urlData.push({
+    id,
+    url,
+  })
+
+  fs.writeFileSync(urlDataPath, JSON.stringify(urlData), 'utf-8')
+
+  res.render('index', { id })
+})
+
+
+function checkRandomId(urlData) {
+  let id = randomGenerator(5)
+  if (urlData.length) {
+    if (urlData.some((url) => url.id === id)) {
+      checkRandomId(urlData)
+    }
+  }
+  return id
+}
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000')
+  console.log('Server is running on http://localhost:3000')
 })
